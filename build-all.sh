23a462d31d4c8435d69da1b33cd6bfe9a1030c28
Nigel Tao <nigeltao@golang.org>
2018-06-16 08:38:29 +1000

    Move build-all.sh to the top dir; add comments

--- build-all.sh
+++ build-all.sh
@@ -13,8 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This is a simple sanity check that the tests pass and the example and fuzz
-# programs compile.
+# This script is a simple sanity check that the tests pass and the example and
+# fuzz programs compile.
+#
+# If you're just looking to get started with Wuffs, running this script isn't
+# necessary (as Wuffs doesn't have the "configure; make; make install" dance or
+# its equivalent), although studying this script can help you learn how the
+# pieces fit together.
+#
+# For example, this script builds command line tools, such as 'wuffs' and
+# 'wuffs-fmt', that are used when *developing* Wuffs, but aren't necessary if
+# all you want to do is *use* Wuffs as a third party library. In the latter
+# case, the only files you need are those under the release/ directory.
+#
+# Instead of running this script, you should be able to run the example
+# programs (except the 'example/library' special case) out of the box, without
+# having to separately configure, build or install a library:
+#
+# git clone https://github.com/google/wuffs.git
+# cd wuffs/example/zcat
+# gcc zcat.c
+# ./a.out < ../../test/data/romeo.txt.gz
 
 go install github.com/google/wuffs/cmd/...
 go test    github.com/google/wuffs/...
