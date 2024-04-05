a9a6d8ac5507bad9ea5a4a08051a9bb489e7bf09
Nigel Tao <nigeltao@golang.org>
2024-04-05 13:30:18 +1100

    base: make malloc_slice_u8(etc, 0) return empty
    Updates #135

--- release/c/wuffs-unsupported-snapshot.c
+++ release/c/wuffs-unsupported-snapshot.c
@@ -3916,12 +3916,12 @@ wuffs_base__token_buffer::writer_token_position() const {
 //
 // You can pass the C stdlib's malloc as the malloc_func.
 //
-// It returns an empty slice (containing a NULL ptr field) if (num_uxx *
-// sizeof(uintxx_t)) would overflow SIZE_MAX.
+// It returns an empty slice (containing a NULL ptr field) if num_uxx is zero
+// or if (num_uxx * sizeof(uintxx_t)) would overflow SIZE_MAX.
 
 static inline wuffs_base__slice_u8  //
 wuffs_base__malloc_slice_u8(void* (*malloc_func)(size_t), uint64_t num_u8) {
-  if (malloc_func && (num_u8 <= (SIZE_MAX / sizeof(uint8_t)))) {
+  if (malloc_func && num_u8 && (num_u8 <= (SIZE_MAX / sizeof(uint8_t)))) {
     void* p = (*malloc_func)((size_t)(num_u8 * sizeof(uint8_t)));
     if (p) {
       return wuffs_base__make_slice_u8((uint8_t*)(p), (size_t)num_u8);
@@ -3932,7 +3932,7 @@ wuffs_base__malloc_slice_u8(void* (*malloc_func)(size_t), uint64_t num_u8) {
 
 static inline wuffs_base__slice_u16  //
 wuffs_base__malloc_slice_u16(void* (*malloc_func)(size_t), uint64_t num_u16) {
-  if (malloc_func && (num_u16 <= (SIZE_MAX / sizeof(uint16_t)))) {
+  if (malloc_func && num_u16 && (num_u16 <= (SIZE_MAX / sizeof(uint16_t)))) {
     void* p = (*malloc_func)((size_t)(num_u16 * sizeof(uint16_t)));
     if (p) {
       return wuffs_base__make_slice_u16((uint16_t*)(p), (size_t)num_u16);
@@ -3943,7 +3943,7 @@ wuffs_base__malloc_slice_u16(void* (*malloc_func)(size_t), uint64_t num_u16) {
 
 static inline wuffs_base__slice_u32  //
 wuffs_base__malloc_slice_u32(void* (*malloc_func)(size_t), uint64_t num_u32) {
-  if (malloc_func && (num_u32 <= (SIZE_MAX / sizeof(uint32_t)))) {
+  if (malloc_func && num_u32 && (num_u32 <= (SIZE_MAX / sizeof(uint32_t)))) {
     void* p = (*malloc_func)((size_t)(num_u32 * sizeof(uint32_t)));
     if (p) {
       return wuffs_base__make_slice_u32((uint32_t*)(p), (size_t)num_u32);
@@ -3954,7 +3954,7 @@ wuffs_base__malloc_slice_u32(void* (*malloc_func)(size_t), uint64_t num_u32) {
 
 static inline wuffs_base__slice_u64  //
 wuffs_base__malloc_slice_u64(void* (*malloc_func)(size_t), uint64_t num_u64) {
-  if (malloc_func && (num_u64 <= (SIZE_MAX / sizeof(uint64_t)))) {
+  if (malloc_func && num_u64 && (num_u64 <= (SIZE_MAX / sizeof(uint64_t)))) {
     void* p = (*malloc_func)((size_t)(num_u64 * sizeof(uint64_t)));
     if (p) {
       return wuffs_base__make_slice_u64((uint64_t*)(p), (size_t)num_u64);
